e639bc01b325486e17f559de32c84748d950abec
Vineeth Vijayan <vineeth@moonraft.com>
2015-10-28 16:01:33 +0530

    commit the new

--- packages/custom/submit/public/controllers/submit.js
+++ packages/custom/submit/public/controllers/submit.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /* jshint -W098 */
-angular.module('mean.submit').controller('SubmitController', ['$scope', 'Global', 'Submit',
-  function($scope, Global, Submit) {
+angular.module('mean.submit').controller('SubmitController', ['$scope', 'Global', 'Submit', '$state',
+  function($scope, Global, Submit, $state) {
     $scope.global = Global;
     $scope.package = {
       name: 'submit'
@@ -23,5 +23,31 @@ angular.module('mean.submit').controller('SubmitController', ['$scope', 'Global'
       startingDay: 1
     };
 
+    $scope.go = function() {
+      debugger;
+      $state.go('landing.searched', {
+        fromDate: $scope.fromDate,
+        toDate: $scope.toDate
+      });
+    }
+
+  }
+]);
+
+
+angular.module('mean.submit').controller('BillsController', ['$scope', 'Global', 'Submit', '$stateParams', '$http',
+  function($scope, Global, Submit, $stateParams, $http) {
+    $http({
+      url: 'http://192.168.1.172:3000/api/addbill/claim',
+      method: 'POST',
+      data: {
+        fromDate: $stateParams,
+        toDate: $stateParams.toDate
+      }
+    }).then(function(data) {
+      $scope.bills = data.unclaimedBills
+    }, function(err) {
+      console.log(err);
+    });
   }
 ]);
--- packages/custom/submit/public/routes/submit.js
+++ packages/custom/submit/public/routes/submit.js
@@ -1,16 +1,22 @@
 'use strict';
 
+angular.module('mean.submit').config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}])
+
 angular.module('mean.submit').config(['$viewPathProvider', function($viewPathProvider) {
   $viewPathProvider.override('system/views/index.html', 'submit/views/index.html');
 }]).config(['$stateProvider',
   function($stateProvider) {
     $stateProvider.state('landing', {
-      url: '/submit/search',
-      templateUrl: 'submit/views/index.html',
-      views:{
-        url:'/submit/search/results',
-        templateUrl:'submit/views/search.results.html'
-      }
-    });
+        url: '/submit/search',
+        templateUrl: 'submit/views/index.html'
+      })
+      .state('landing.searched', {
+        url: '/:fromDate/:toDate',
+        templateUrl: 'submit/views/search.results.html',
+        controller: 'BillsController'
+      });
   }
 ]);
